d1beec65ea1eb7ff759f5710f1fca02d34661001
Martin Streicher <martin.streicher@gmail.com>
2012-05-21 09:15:59 -0400

    WIP

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -1,6 +1,9 @@
 require 'spec_helper'
 
 describe AnswersController do
+  describe 'Methods' do
+  end
+  
   describe 'Private Methods' do
     let(:ps)  { %w(player1 player3) }
     let(:p)   { create :play, players: ps }
@@ -11,7 +14,6 @@ describe AnswersController do
         assigns[:play].id.should == p.id
         assigns[:player].should == 'player1'
         assigns[:answer].should == 'banana'
-        response.location.should_not == nil
       end
     end
   end
