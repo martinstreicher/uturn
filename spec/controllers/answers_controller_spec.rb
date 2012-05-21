4e6217cd2943a57b0be2a4b8dca824b23fcce7ef
Martin Streicher <strike@paige.(none)>
2012-05-21 09:13:26 -0400

    Add answers controller.

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -7,9 +7,11 @@ describe AnswersController do
     
     describe '.fetch_params' do
       it 'assigns @play and @player var based on ID params' do
-        post :create, play_id: p.id, player_id: ps.first
+        post :create, play_id: p.id, player_id: ps.first, answer: 'banana'
         assigns[:play].id.should == p.id
         assigns[:player].should == 'player1'
+        assigns[:answer].should == 'banana'
+        response.location.should_not == nil
       end
     end
   end
